863a66938f51c95b323728eb578964ea5354ee54
icw <para@uniess.co.kr>
2022-08-30 22:47:18 +0900

    annotation

---
+++ src/main/java/com/example/test/class_/toString/t2/C1.java
@@ -0,0 +1,8 @@
+package com.example.test.class_.toString.t2;
+
+import lombok.ToString;
+
+@ToString
+public class C1 {
+	String name = "asd";
+}
---
+++ src/main/java/com/example/test/class_/toString/t2/Run.java
@@ -0,0 +1,11 @@
+package com.example.test.class_.toString.t2;
+
+public class Run {
+	public static void main(String[] args) {
+
+		C2 c2 = new C2();
+
+		System.out.println(c2);
+
+	}
+}
